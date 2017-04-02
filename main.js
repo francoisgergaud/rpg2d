b075040144e2dfe3573369e3dd01145f6ca0e08a
François Gergaud <francoisgergaud@MacBook-Pro-de-Francois.local>
2017-04-01 21:24:37 -0600

    refactor code: clean and improve character animation

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var gridBlockSize = 20;
 var refreshPeriod = 50;
 var animationPeriod = 50;
 
+/**
+ * the character class
+ */
 var Character = {
 	spriteCanvas: null,
 	destinationCanvas: null,
@@ -19,75 +22,78 @@ var Character = {
 		velocity: 5,
 		moving: false
 	},
-	animationData:{
-		up:[],
-		down:[],
-		left:[],
-		right:[]
+	animationData:[],
+	spriteLoading: false,
+
+	initializeCharacterProperties: function(destinationCanvas, backgroundCanvas, spritesFilename){
+		this.backgroundCanvas = backgroundCanvas;
+	   	this.destinationCanvas = destinationCanvas;
+   		var animationData = [
+   			[{x:6,y:0},{x:7,y:0},{x:8,y:0}],
+   			[{x:6,y:2},{x:7,y:2},{x:8,y:2}],
+   			[{x:6,y:3},{x:7,y:3},{x:8,y:3}],
+   			[{x:6,y:1},{x:7,y:1},{x:8,y:1}]
+   		];
+		this.loadSprites(spritesFilename, animationData);
+		this.animationData = animationData;
 	},
+
+	loadSprites: function(filename){
+		this.spriteCanvas = document.createElement('canvas');
+		var spriteCanvasContext = this.spriteCanvas.getContext('2d');
+		drawing = new Image();
+		drawing.onload = function() {
+	   		spriteCanvasContext.drawImage(drawing,0,0);
+	   		this.spriteLoading = false;
+		}.bind(this);
+		this.spriteLoading = true;
+		drawing.src = filename;
+	},
+
 	animate: function(){
 		if(this.currentState.moving){
 			var sprites = null;
-			if(this.currentState.direction == 0){
-				this.currentState.frame = (this.currentState.frame+1)%this.animationData.down.length;
-				this.currentState.position.y +=this.currentState.velocity;
-				sprites = this.animationData.down;
-			}else if(this.currentState.direction == 1){
-				this.currentState.frame = (this.currentState.frame+1)%this.animationData.right.length;
-				this.currentState.position.x +=this.currentState.velocity;
-				sprites = this.animationData.right;
-			}else if(this.currentState.direction == 2){
-				this.currentState.frame = (this.currentState.frame+1)%this.animationData.up.length;
-				this.currentState.position.y -=this.currentState.velocity;
-				sprites = this.animationData.up;
-			}else if(this.currentState.direction == 3){
-				this.currentState.frame = (this.currentState.frame+1)%this.animationData.left.length;
-				this.currentState.position.x -=this.currentState.velocity;
-				sprites = this.animationData.left;
+			switch(this.currentState.direction){
+				case 0 :
+					this.currentState.position.y +=this.currentState.velocity;
+					break;
+				case 1:
+					this.currentState.position.x +=this.currentState.velocity;
+					break;
+				case 2:
+					this.currentState.position.y -=this.currentState.velocity;
+					break;
+				case 3:
+					this.currentState.position.x -=this.currentState.velocity;
+					break;
 			}
-			this.drawSprite(sprites[this.currentState.frame].x,sprites[this.currentState.frame].y);
+			//TODO review the sprites array not initializeds
+			this.currentState.frame = (this.currentState.frame+1)%this.animationData[this.currentState.direction].length;
+			this.drawSprite();
 		}
 	},
-	registerEvents: function(){
-		var animationTimer = null;
-		var character = this;
-		window.addEventListener(
-			'keydown',
-			function(e){
-				switch(e.keyCode){
-					case 40: character.currentState.direction=0; break;
-					case 39: character.currentState.direction=1; break;
-					case 38: character.currentState.direction=2; break;
-					case 37: character.currentState.direction=3; break;
-				}
-				character.currentState.moving = true;
-			});
-		window.addEventListener(
-			'keyup',
-			function(e){
-				character.currentState.moving = false;
-			});
-		var animationTimer = window.setInterval(character.animate.bind(character), animationPeriod);
-			
-	},
-	drawSprite : function(spriteRowNumber, spriteColumnNumber){
-		var spriteSize = 32;
-		this.destinationCanvas.getContext('2d').drawImage(
-			this.backgroundCanvas,
-			0,
-			0
-		);
-		this.destinationCanvas.getContext('2d').drawImage(
-			this.spriteCanvas,
-			spriteRowNumber*spriteSize, 
-			spriteColumnNumber*spriteSize,
-			spriteSize,
-			spriteSize, 
-			this.currentState.position.x, 
-			this.currentState.position.y, 
-			spriteSize,
-			spriteSize
-		);
+
+	drawSprite : function(){
+		if(!this.spriteLoading){
+			var spriteSize = 32;
+			this.destinationCanvas.getContext('2d').drawImage(
+				this.backgroundCanvas,
+				0,
+				0
+			);
+			spriteCoordinate = this.animationData[this.currentState.direction][this.currentState.frame];
+			this.destinationCanvas.getContext('2d').drawImage(
+				this.spriteCanvas,
+				spriteCoordinate.x*spriteSize, 
+				spriteCoordinate.y*spriteSize,
+				spriteSize,
+				spriteSize, 
+				this.currentState.position.x, 
+				this.currentState.position.y, 
+				spriteSize,
+				spriteSize
+			);
+		}
 	}
 }
 
@@ -103,31 +109,47 @@ function createBufferCanvasFronDisplayCanvas(displayCanvas){
 	return backgroundCanvas;
 }
 
-function loadSprites(spriteFilePath, destinationCanvas, backgroundCanvas){
-	var spriteCanvas = document.createElement('canvas');
-	var context=spriteCanvas.getContext('2d');
-	drawing = new Image();
-	drawing.onload = function() {
-   		context.drawImage(drawing,0,0);
-   		var firstCharacter = new Object(Character);
-   		firstCharacter.spriteCanvas = spriteCanvas;
-   		firstCharacter.backgroundCanvas = backgroundCanvas;
-   		firstCharacter.destinationCanvas = destinationCanvas;
-   		firstCharacter.animationData.down = [{x:6,y:0},{x:7,y:0},{x:8,y:0}];
-   		firstCharacter.animationData.left =[{x:6,y:1},{x:7,y:1},{x:8,y:1}];
-   		firstCharacter.animationData.right = [{x:6,y:2},{x:7,y:2},{x:8,y:2}];
-   		firstCharacter.animationData.up = [{x:6,y:3},{x:7,y:3},{x:8,y:3}];
-		firstCharacter.registerEvents();
-	};
-	drawing.src = spriteFilePath;
-
+function registerEventForPlayableCharacter(character){
+	var animationTimer = null;
+	window.addEventListener(
+		'keydown',
+		function(e){
+			switch(e.keyCode){
+				case 40:
+					//down
+					character.currentState.direction=0; 
+					break;
+				case 39: 
+					//right
+					character.currentState.direction=1;
+					break;
+				case 38:
+					//up
+					character.currentState.direction=2; 
+					break;
+				case 37: 
+					//left
+					character.currentState.direction=3; 
+					break;
+			}
+			character.currentState.moving = true;
+		}
+	);
+	window.addEventListener(
+		'keyup',
+		function(e){
+			character.currentState.moving = false;
+		}
+	);
 }
-
 
 function game(canvas){
 	var timer;
 	canvas.width = 640;
 	canvas.height = 480;
 	var backgroundCanvas = createBufferCanvasFronDisplayCanvas(canvas);
-	var spritesCanvas = loadSprites("./resources/hetalia_sprites_by_carmenmcs.png", canvas, backgroundCanvas);
+	var playableCharacter = new Object(Character);
+	playableCharacter.initializeCharacterProperties(canvas, backgroundCanvas, "./resources/hetalia_sprites_by_carmenmcs.png");
+	registerEventForPlayableCharacter(playableCharacter);
+	var animationTimer = window.setInterval(playableCharacter.animate.bind(playableCharacter), animationPeriod);
 }
