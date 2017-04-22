4f4e3c5f03bf6ebebdd325d12d05a1f577666a3b
François Gergaud <francoisgergaud@MacBook-Pro-de-Francois.local>
2017-04-21 19:10:29 -0500

    buffer-canvas scrolling: first draft

--- renderer.js
+++ renderer.js
@@ -31,8 +31,14 @@ function Renderer (animatedElements, environment, displayCanvas, viewPort){
 	this._createBufferCanvas = function(){
 		this._backgroundCanvas = document.createElement('canvas');
 		//this._backgroundCanvas.setAttribute("id", "_backgroundCanvas");
-		this._backgroundCanvas.width = (this._environment.spriteSize*50);
-		this._backgroundCanvas.height = (this._environment.spriteSize*50);
+		this._backgroundCanvas.width = this._displayCanvas.width+this._environment.spriteSize;
+		this._backgroundCanvas.height = this._displayCanvas.height+this._environment.spriteSize;
+		this.backgroundViewPort={
+			x: 0,
+			y: 0,
+			width: (parseInt(this._displayCanvas.width/this._environment.spriteSize))+1,
+			height: (parseInt(this._displayCanvas.height/this._environment.spriteSize))+1,
+		};
 	};
 
 	/**
@@ -44,33 +50,21 @@ function Renderer (animatedElements, environment, displayCanvas, viewPort){
 		if(!this._environment.spriteLoading){
 			if(this._isBackgroungCanvasReady){
 				//refresh the background
+				var xOffset = this._viewPort.x-(this.backgroundViewPort.x*this._environment.spriteSize);
+				var yOffset = this._viewPort.y-(this.backgroundViewPort.y*this._environment.spriteSize);
 				this._displayCanvas.getContext('2d').drawImage(
 					this._backgroundCanvas,
-					this._viewPort.x,
-					this._viewPort.y,
+					xOffset,
+					yOffset,
 					this._viewPort.width,
 					this._viewPort.height,
 					0,
 					0,
 					this._viewPort.width,
-					this._viewPort.height);
+					this._viewPort.height
+				);
 			}else{
-				//initialize the background canvas from the grid
-				for(i = 0; i < this._environment.grid.length; i++){
-					for(j= 0; j < this._environment.grid[i].length; j++){
-						this._backgroundCanvas.getContext('2d').drawImage(
-							this._environment.spriteCanvas,
-							this._environment.spritesData[this._environment.grid[i][j].spriteId].x*this._environment.spriteSize, 
-							this._environment.spritesData[this._environment.grid[i][j].spriteId].y*this._environment.spriteSize,
-							this._environment.spriteSize,
-							this._environment.spriteSize, 
-							i * this._environment.spriteSize, 
-							j * this._environment.spriteSize, 
-							this._environment.spriteSize,
-							this._environment.spriteSize
-						);
-					}
-				}
+				this.initializeBackgroundCanvas();
 				//document.body.appendChild(this._backgroundCanvas);
 				this._isBackgroungCanvasReady = true;
 			}
@@ -113,12 +107,176 @@ function Renderer (animatedElements, environment, displayCanvas, viewPort){
 	 * @return {None}
 	 */
 	this.moveViewPort = function(x, y, xOffset, yOffset){
-		if(x > this._viewPort.width/2 && x < (this._backgroundCanvas.width-this._viewPort.width/2)){
+		var maxWidth = this._environment.grid.length*this._environment.spriteSize;
+		var maxHeight = this._environment.grid[0].length*this._environment.spriteSize;
+		var viewPortCenterX = this._viewPort.width/2 ;
+		var viewPortCenterY = this._viewPort.height/2 ;
+		if(x > viewPortCenterX  && x < (maxWidth-viewPortCenterX)){
+			var xSprtieDistance = parseInt((this._viewPort.x+xOffset)/this._environment.spriteSize) - parseInt(this._viewPort.x/this._environment.spriteSize);
+			if(xSprtieDistance == 1){
+				//move background-canvas to right
+				this.moveBackgroundCanvasToRight();
+			}
+			if(xSprtieDistance == -1){
+				//move background-canvas to right
+				this.moveBackgroundCanvasToLeft();
+			}
 			this._viewPort.x+=xOffset;
 		}
-		if(y > this._viewPort.height/2 && y < (this._backgroundCanvas.height-this._viewPort.height/2)){
+		if(y > viewPortCenterY && y < (maxHeight-viewPortCenterY)){
+			var ySprtieDistance = parseInt((this._viewPort.y+yOffset)/this._environment.spriteSize) - parseInt(this._viewPort.y/this._environment.spriteSize);
+			if(ySprtieDistance == 1){
+				//move background-canvas to right
+				this.moveBackgroundCanvasToBottom();
+			}
+			if(ySprtieDistance == -1){
+				//move background-canvas to right
+				this.moveBackgroundCanvasToTop();
+			}
 			this._viewPort.y+=yOffset;
 		}
+	};
+
+	/**
+	 * move the background canvas to the right 
+	 * @return {[type]} [description]
+	 */
+	this.initializeBackgroundCanvas = function(){
+		for(i = 0; i < this.backgroundViewPort.width; i++){
+			for(j= 0; j < this.backgroundViewPort.height; j++){
+				this._backgroundCanvas.getContext('2d').drawImage(
+					this._environment.spriteCanvas,
+					this._environment.spritesData[this._environment.grid[i][j].spriteId].x*this._environment.spriteSize, 
+					this._environment.spritesData[this._environment.grid[i][j].spriteId].y*this._environment.spriteSize,
+					this._environment.spriteSize,
+					this._environment.spriteSize, 
+					i * this._environment.spriteSize, 
+					j * this._environment.spriteSize, 
+					this._environment.spriteSize,
+					this._environment.spriteSize
+				);
+			}
+		}
+	};
+
+	/**
+	 * move the background canvas to the right 
+	 * @return {[type]} [description]
+	 */
+	this.moveBackgroundCanvasToRight = function(){
+		//move background-canvas to left
+		this._backgroundCanvas.getContext('2d').drawImage(
+			this._backgroundCanvas,
+			0-this._environment.spriteSize,
+			0
+		);
+		//and add the missing right part
+		var gridColumnIndex = this.backgroundViewPort.x+this.backgroundViewPort.width -1;
+		for(j=0; j< this._environment.grid[gridColumnIndex].length; j++){
+			//move background-canvas to left
+			this._backgroundCanvas.getContext('2d').drawImage(
+				this._environment.spriteCanvas,
+				this._environment.spritesData[this._environment.grid[gridColumnIndex][j].spriteId].x*this._environment.spriteSize, 
+				this._environment.spritesData[this._environment.grid[gridColumnIndex][j].spriteId].y*this._environment.spriteSize,
+				this._environment.spriteSize,
+				this._environment.spriteSize, 
+				(this.backgroundViewPort.width - 1) * this._environment.spriteSize, 
+				j * this._environment.spriteSize, 
+				this._environment.spriteSize,
+				this._environment.spriteSize
+			);
+		}
+		this.backgroundViewPort.x++;
+	};
+
+	/**
+	 * move the background canvas to the left 
+	 * @return {None}
+	 */
+	this.moveBackgroundCanvasToLeft = function(){
+		//move background-canvas to left
+		this._backgroundCanvas.getContext('2d').drawImage(
+			this._backgroundCanvas,
+			this._environment.spriteSize,
+			0
+		);
+		//and add the missing right part
+		var gridColumnIndex = this.backgroundViewPort.x;
+		for(j=0; j< this._environment.grid[gridColumnIndex].length; j++){
+			//move background-canvas to left
+			this._backgroundCanvas.getContext('2d').drawImage(
+				this._environment.spriteCanvas,
+				this._environment.spritesData[this._environment.grid[gridColumnIndex][j].spriteId].x*this._environment.spriteSize, 
+				this._environment.spritesData[this._environment.grid[gridColumnIndex][j].spriteId].y*this._environment.spriteSize,
+				this._environment.spriteSize,
+				this._environment.spriteSize, 
+				0,
+				j * this._environment.spriteSize, 
+				this._environment.spriteSize,
+				this._environment.spriteSize
+			);
+		}
+		this.backgroundViewPort.x--;
+	};
+
+	/**
+	 * move the background canvas to the bottom 
+	 * @return {None}
+	 */
+	this.moveBackgroundCanvasToBottom = function(){
+		//move background-canvas to left
+		this._backgroundCanvas.getContext('2d').drawImage(
+			this._backgroundCanvas,
+			0,
+			0-this._environment.spriteSize
+		);
+		//and add the missing right part
+		var gridLineIndex = this.backgroundViewPort.y+this.backgroundViewPort.height -1;
+		for(i=0; i< this._environment.grid.length; i++){
+			//move background-canvas to left
+			this._backgroundCanvas.getContext('2d').drawImage(
+				this._environment.spriteCanvas,
+				this._environment.spritesData[this._environment.grid[i][gridLineIndex].spriteId].x*this._environment.spriteSize, 
+				this._environment.spritesData[this._environment.grid[i][gridLineIndex].spriteId].y*this._environment.spriteSize,
+				this._environment.spriteSize,
+				this._environment.spriteSize, 
+				i * this._environment.spriteSize, 
+				(this.backgroundViewPort.height - 1) * this._environment.spriteSize, 
+				this._environment.spriteSize,
+				this._environment.spriteSize
+			);
+		}
+		this.backgroundViewPort.y++;
+	};
+
+	/**
+	 * move the background canvas to the top 
+	 * @return {None}
+	 */
+	this.moveBackgroundCanvasToTop = function(){
+		//move background-canvas to left
+		this._backgroundCanvas.getContext('2d').drawImage(
+			this._backgroundCanvas,
+			0,
+			this._environment.spriteSize
+		);
+		//and add the missing right part
+		var gridLineIndex = this.backgroundViewPort.y-1;
+		for(i=0; i< this._environment.grid.length; i++){
+			//move background-canvas to left
+			this._backgroundCanvas.getContext('2d').drawImage(
+				this._environment.spriteCanvas,
+				this._environment.spritesData[this._environment.grid[i][gridLineIndex].spriteId].x*this._environment.spriteSize, 
+				this._environment.spritesData[this._environment.grid[i][gridLineIndex].spriteId].y*this._environment.spriteSize,
+				this._environment.spriteSize,
+				this._environment.spriteSize, 
+				i * this._environment.spriteSize, 
+				0, 
+				this._environment.spriteSize,
+				this._environment.spriteSize
+			);
+		}
+		this.backgroundViewPort.y--;
 	};
 
 	/**
